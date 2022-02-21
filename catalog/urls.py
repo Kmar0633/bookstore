400de70ee72093e3d0a15bbaff080757ee528479
Kmar0633 <kmar0633@uni.sydney.edu.au>
2022-02-21 15:03:40 +1100

    refactored urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-
-]
-
-
-urlpatterns += [
     path('', views.index, name='index'),
- 
     path('delete_book/<book_id>', views.delete_book, name='delete-book'),
     path('add_book/', views.add_book, name='add-book'),
     path('add_profile_picture/<user_id>', views.add_profilepicture, name='add-profilepicture'),
-    path('error_page/', views.index, name='error-page'),
-
+    path('error_page/', views.index, name='error-page')
  
 ]
 
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -23,17 +23,11 @@ from django.conf.urls.static import static
 from catalog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('catalog/', include('catalog.urls')),
+      path('', RedirectView.as_view(url='catalog/', permanent=True)),
+      path('catalog/', include('django.contrib.auth.urls')),
 ]
 
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
 
-urlpatterns += [
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-]
-urlpatterns += [
-    path('catalog/', include('django.contrib.auth.urls')),
-]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
